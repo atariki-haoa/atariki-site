3235cee0ef1c3ea0221956108385a24dbc692776
Ariel Lobos <ariel@atariki.com>
2024-09-29 17:31:06 -0300

    Add title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <title>Ariel Atariki Lobos Haoa</title>
           <link
             href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@400;500;700&display=swap"
             rel="stylesheet"
