bc7cb8a9175c874bf41fccf89f3d5f2467a02b80
Ariel Lobos <ariel@atariki.com>
2024-10-02 23:51:14 -0300

    Remove env

--- next.config.js
+++ next.config.js
@@ -6,6 +6,5 @@ module.exports = {
     MAILGUN_DOMAIN: process.env.MAILGUN_DOMAIN,
     LLM_API_URL: process.env.LLM_API_URL,
     CSRF_SECRET: process.env.CSRF_SECRET,
-    NODE_ENV: process.env.NODE_ENV,
   },
 };
