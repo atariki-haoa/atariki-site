31cab8886c1ce88237a579c24f426070b57904da
Ariel Lobos <ariel@atariki.com>
2025-09-23 22:40:26 -0300

    Remove chat feature and dist folder (#2)

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ module.exports = {
   env: {
     MAILGUN_API_KEY: process.env.MAILGUN_API_KEY,
     MAILGUN_DOMAIN: process.env.MAILGUN_DOMAIN,
-    LLM_API_URL: process.env.LLM_API_URL,
     CSRF_SECRET: process.env.CSRF_SECRET,
   },
 };
